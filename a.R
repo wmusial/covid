460047fb60795d356ab55d5e5b4adacbc0989703
Voitek Musial <voitek@voleon.com>
2020-04-29 01:18:04 -0700

    init

---
+++ a.R
@@ -0,0 +1,55 @@
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(VORtex)
+
+data.dt = as.data.table(read.csv("covid-confirmed-deaths-since-5th-death.csv"))
+names(data.dt) =  c('entity', 'code', 'date', 'deaths', 'days.since')
+data.dt[, date := parse_date_time(as.character(date), orders="bdy")]
+
+setkey(data.dt, entity, date)
+data.dt[, d.deaths := deaths - VORtex::ts.lag(deaths, 1), by = entity]
+
+# print total deaths summary
+max.deaths.dt = data.dt[, list(max.deaths=max(deaths)), by = entity]
+
+writelog("summary of max.deaths:")
+summary(max.deaths.dt$max.deaths)
+
+writelog("entities: %s", length(unique(data.dt$entity)))
+
+sub.data.dt = data.dt[is.element(entity, max.deaths.dt[max.deaths >= 500]$entity)]
+writelog("entities with more than 500 deaths: %s", length(unique(sub.data.dt$entity)))
+
+non.countries = c("Africa", "Asia", "Asia excl. China", "Europe", "European Union", "High income", "Lower middle income", "North America", "South America", "Upper middle income", "World", "World excl. China", "World excl. China and South Korea", "World excl. China, South Korea, Japan and Singapore")
+sub.data.dt = sub.data.dt[!is.element(entity, non.countries)]
+
+sub.data.dt[, week := as.integer((.N - seq(.N)) / 7), by = entity]
+byweek.dt = sub.data.dt[, list(d.deaths = sum(d.deaths)), keyby = list(entity, week)]
+
+do.plot(byweek.dt, "week", "ourworldindata_500_countries_revweek.pdf")
+
+
+peak.week.dt = byweek.dt[, list(peak.week = which.max(d.deaths)), by = entity]
+
+peak.countries = peak.week.dt[peak.week >= 3, entity]
+
+peak.dt = sub.data.dt[is.element(entity, peak.countries)]
+do.plot(peak.dt, "date", "ourworldindata_500_countries_oldpeak.pdf")
+
+# clean outliers
+peak.dt[d.deaths == 0, d.deaths := NA]
+peak.dt[, mean.d.deaths := ts.lag(ts.roll.mean(d.deaths, 3), 1), by = entity]
+peak.dt[, mean.d.deaths.lag := ts.lag(mean.d.deaths, -2), by = entity]
+
+
+do.plot = function(data.dt, x.name, path = NULL) {
+	if (!is.null(path)) {
+		pdf(width=10, height=3*length(unique(data.dt$entity)), file=path)
+  }
+	p = ggplot(data.dt, aes(x=get(x.name), y=d.deaths)) + facet_grid(entity~., scales="free") + geom_step() + labs(x=x.name)
+	print(p)
+	if (!is.null(path)) {
+		dev.off()
+  }
+}
---
+++ report.md
@@ -0,0 +1,95 @@
+# data
+
+## ourworldindata.org
+
+downloaded from:
+https://ourworldindata.org/coronavirus
+
+deaths since 5th confirmed death
+
+## point1acres
+
+Leigh's data:
+https://instant.1point3acres.com/v1/api/coronavirus/us/cases?token=XXX
+
+file cases.csv
+
+This data only has US states
+
+# eda
+
+We look at the data from ourworldindata.org
+
+[ourworldindata_all.pdf](ourworldindata_all.pdf)
+
+That's a lot of countries with little deaths.
+In our eventual fit, we will weigh by death numbers,
+so we might as well throw out countries with few deaths at little cost.
+
+
+```
+> summary(max.deaths.dt$max.deaths)
+    Min.  1st Qu.   Median     Mean  3rd Qu.     Max.     NA's
+     1.0      7.0     30.0   8637.0    293.5 210200.0        2
+```
+
+all entities: 186
+entities, >100 deaths: 66
+entities >500 deaths: 41
+
+The plot again, just with >500 deaths:
+[ourworldindata_500.pdf](ourworldindata_500.pdf)
+
+non-countries amongst the 41:
+
+```
+Africa
+Asia
+Asia excl. China
+Europe
+European Union
+High income
+Lower middle income
+North America
+South America
+Upper middle income
+World
+World excl. China
+World excl. China and South Korea
+World excl. China, South Korea, Japan and Singapore
+```
+
+left over: 27
+
+[ourworldindata_500_countries.pdf](ourworldindata_500_countries.pdf)
+
+We want to choose countries that already peaked.
+We plot weekly deaths, counting weeks from the end,
+and pick countries in which the peak occurs on week index 2 or more (ie chronologically earlier).
+
+"old peak" Countries:
+```
+Austria
+Belgium
+Ecuador
+France
+Iran
+Italy
+Netherlands
+Philippines
+Portugal
+Spain
+Switzerland
+United Kingdom
+```
+
+[ourworldindata_500_countries.pdf](ourworldindata_500_countries_oldpeak.pdf)
+
+
+We clean out outliers:
+
+* zero observations
+* more than twice the mean of past three days
+
+
+
